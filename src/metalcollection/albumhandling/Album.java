e44817ce3eec72fd50138eca72730220d2eed32e
Souvatzis Vasilios <vasouv@gmail.com>
2013-11-02 21:20:34 +0200

    Album class creation
    Created the Album class, added JDBC driver and SQLite DB.

--- src/metalcollection/albumhandling/Album.java
+++ src/metalcollection/albumhandling/Album.java
@@ -6,10 +6,66 @@
 
 package metalcollection.albumhandling;
 
+import java.io.Serializable;
+
 /**
  *
  * @author vasouv
  */
-public class Album {
+public class Album implements Serializable {
+    private int id;
+    private String band;
+    private String title;
+    private int release;
+    private String genre;
+    
+    public Album(int id, String band, String title, int release, String genre) {
+        this.id = id;
+        this.band = band;
+        this.title = title;
+        this.release = release;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRelease() {
+        return release;
+    }
+
+    public void setRelease(int release) {
+        this.release = release;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
     
 }
